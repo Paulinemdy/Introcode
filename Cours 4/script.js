92089d4f1475305b04cf363e61c0b8956bf6b226
Pauline <j.pauline-m@hotmail.fr>
2018-04-10 11:42:41 +0200

    Cours 4 Pauline Mondy

---
+++ Cours 4/script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+
+function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+};
+welcome ();
+
+
+function getUsername() {
+    //affiche une boite de dialogue 
+    var  user = prompt("Veuillez rentrez votre nom:", "");
+    // la fonction se termine en retournant le nom récupéré par la boite de dialogue
+    return user
+}
+});
